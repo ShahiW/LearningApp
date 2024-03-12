b0a47f58d3c53a0b514b54c0ed237d2c96c17af8
Shahi Winderling <s.winderling@posteo.de>
2024-03-12 23:17:08 +0100

    finish login, logout and registration

--- LearningApp/quiz/urls.py
+++ LearningApp/quiz/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 
-
 urlpatterns = [
     path("", views.quiz, name="quiz"),
-    path("login", views.loginSeite, name="login"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
     path("registration", views.registration, name="registration"),
 ]
