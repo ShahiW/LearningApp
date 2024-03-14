e7e29d97a1a3843848372a3876be0ff2b58d4b5a
Shahi Winderling <s.winderling@posteo.de>
2024-03-14 12:00:35 +0100

    add classes in models.py

--- LearningApp/quiz/models.py
+++ LearningApp/quiz/models.py
@@ -1,4 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Schüler(models.Model):
+    schüler = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    vorname = models.CharField(max_length=200, null=True)
+    nachname = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    passwort = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.vorname
+    
+
+class Lehrer(models.Model):
+    leherer = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    vorname = models.CharField(max_length=200, null=True)
+    nachname = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    passwort = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.vorname
+    
+
+class Fach(models.Model):
+    name = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return self.name
+    
 
