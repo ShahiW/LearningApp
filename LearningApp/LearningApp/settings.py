b56ba700ef6568dcc9817ec50f14888d820f9d9b
Shahi Winderling <s.winderling@posteo.de>
2024-03-18 13:20:14 +0100

    add model users, add registration template and view

--- LearningApp/LearningApp/settings.py
+++ LearningApp/LearningApp/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'quiz.apps.QuizConfig',
+    "django.contrib.admin",
+    "quiz.apps.QuizConfig",
+    "users.apps.UsersConfig",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
 ]
 
 MIDDLEWARE = [
