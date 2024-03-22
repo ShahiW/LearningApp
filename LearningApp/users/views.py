b4b22df96525bde72877303981b006cadacd75e3
Shahi Winderling <s.winderling@posteo.de>
2024-03-22 08:53:26 +0100

    add login-required-decorater in users > views.py and set new login-url in settings.py

--- LearningApp/users/views.py
+++ LearningApp/users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 
@@ -16,5 +17,6 @@ def register(request):
     return render(request, 'users/register.html', {'form': form})
 
 
+@login_required
 def profile(request):
     return render(request, 'users/profile.html')
