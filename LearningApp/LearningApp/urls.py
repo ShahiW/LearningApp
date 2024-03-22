68333b5e415da6eb13905f161d2ef219a0d8db14
Shahi Winderling <s.winderling@posteo.de>
2024-03-22 12:00:56 +0100

    add media

--- LearningApp/LearningApp/urls.py
+++ LearningApp/LearningApp/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include 
+from django.conf import settings
+from django.conf.urls.static import static
 from users import views as user_views
 from django.contrib.auth import views as auth_views
 
@@ -28,3 +30,6 @@ urlpatterns = [
     path("register/", user_views.register, name="register"),
     path("", include("quiz.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
