25a97c8a430a19eb5bf0bdc662bc6b9495195a27
Shahi Winderling <s.winderling@posteo.de>
2024-04-17 12:15:34 +0200

    destroyed migrations2

--- LearningApp/quiz/models.py
+++ LearningApp/quiz/models.py
@@ -1,24 +1,24 @@
 from django.db import models
-#from django.utils import timezone
-#from django.contrib.auth.models import User
+import uuid
+import random
 
 # Merke: nach jedem neuen Model makemigrations und migrate!!!
-
-# Beispiel:
-# class Post(models.Model):
-#     title = models.CharField(max_length=100)
-#     content = models.TextField()
-#     date_posted = models.DateTimeField(default=timezone.now)  # keine klammern: hier soll die Funktion noch nicht ausgeführt werden
-#     author = models.ForeignKey(User, on_delete=models.CASCADE)
-
-#     def __str__(self):
-#         return self.title
 
 
 # Meine Models
 
+# Quiz Model
+class BaseModel(models.Model):
+    uid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=True)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
 # Tabelle Fächer
-class Subject(models.Model):
+class Subject(BaseModel):
     name = models.CharField(max_length=100)
 
     def __str__(self):
@@ -26,7 +26,7 @@ class Subject(models.Model):
     
 
 # Tabelle Kategorien
-class Categories(models.Model):
+class Category(BaseModel):
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
 
@@ -34,14 +34,47 @@ class Categories(models.Model):
         return self.name
     
 
-# Tabelle Fragen und Antworten
-class QuestionAnswer(models.Model):
-    quiz = models.ForeignKey(Categories, on_delete=models.CASCADE)
-    question = models.TextField()
-    answer = models.TextField()
+# Tabelle Fragen
+class Question(BaseModel):
+    laq = models.ForeignKey(Category, related_name="laq", on_delete=models.CASCADE)
+    question = models.CharField(max_length=100)
+    marks = models.IntegerField(default=5)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.question
+
+    def get_answers(self):
+        answer_objs = list(Answer.objects.filter(question=self))
+        data = []
+        random.shuffle(answer_objs)
+
+        for answer_obj in answer_objs:
+            data.append(
+                {"answer": answer_obj.answer, "is_correct": answer_obj.is_correct}
+            )
+        return data
+    
+
+# Tabelle Antworten
+class Answer(BaseModel):
+    question = models.ForeignKey(
+        Question, related_name="question_answer", on_delete=models.CASCADE
+    )
+    answer = models.CharField(max_length=100)
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return self.answer
+
+
+# Tabelle Fragen und Antworten
+#class QuestionAnswer(models.Model):
+    #quiz = models.ForeignKey(Category, on_delete=models.CASCADE)
+    #question = models.TextField()
+    #answer = models.TextField()
+
+    #def __str__(self):
+        #return self.question
 
 
 
--- LearningApp/quiz/views.py
+++ LearningApp/quiz/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Categories, Subject
+from .models import Category, Subject
 from django.http import Http404
 
 #from django.contrib.auth import authenticate, login, logout
@@ -25,14 +25,15 @@ def about(request):
     return render(request, "quiz/about.html")
 
 
-def categories(request, subject: str):
+def category(request, subject: str):
     if Subject.objects.filter(name__iexact=subject).exists():
         context = {
-            'categories': Categories.objects.filter(subject__name__iexact=subject),  # iexact um upper-lower-case zu ignorieren
+            'categories': Category.objects.filter(subject__name__iexact=subject),  # iexact um upper-lower-case zu ignorieren
             'subject': subject
         }
 
-        return render(request, "quiz/quizzing.html", context)
+        return render(request, "quiz/categories.html", context)
 
     else:
         raise Http404('Ooops! Diese Seite existiert leider nicht.')
+    
