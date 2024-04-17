aff59df8da7a824a6db46bd2ddeb1eacf70c0d0a
Shahi Winderling <s.winderling@posteo.de>
2024-04-17 15:28:41 +0200

    change image mode from jpg to png

--- LearningApp/users/models.py
+++ LearningApp/users/models.py
@@ -6,7 +6,7 @@ from PIL import Image  # aus Pillow Lib importiere die Klasse Image
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     # for profile picture
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    image = models.ImageField(default='default.png', upload_to='profile_pics')
 
     def __str__(self):
         return f'{self.user.username} Profil'
@@ -14,15 +14,20 @@ class Profile(models.Model):
     def save(self, *args, **kwargs):
         # Speichere die Bilddatei. Das wäre eh passiert. Aber es soll noch eine Funktionalität ergänzt werden.
         super(Profile, self).save(*args, **kwargs)  
-
+        
         # Öffne die Bilddatei
-        imgage = Image.open(self.image.path)  
+        image = Image.open(self.image.path) 
+        # Checke Bilddateityp
+        if image.mode in ('JPG'):
+        # Konvertiere Image von JPG zu PNG
+            image = image.convert('RGB')
 
+    
         # Check ob Bildgröße der Bedingung enspricht:
-        if imgage.height > 300 or imgage.width > 300:
+        if image.height > 100 or image.width > 100:
             # Tuple der Max-Größe, kann in Zukunft einfach angepasst werden
             output_size = (300, 300)
             # Mache dieses Bild zu einem Thumbnail
-            imgage.thumbnail(output_size)
+            image.thumbnail(output_size)
             # Überschreibe die Bilddatei mit dem Thumbnail
-            imgage.save(self.image.path)
+            image.save(self.image.path)
