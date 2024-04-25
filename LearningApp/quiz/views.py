2609d3d3fd3c0e6e77de1e8b74fdd35f380a3ab2
Shahi Winderling <s.winderling@posteo.de>
2024-04-25 15:28:06 +0200

    add login required decorator

--- LearningApp/quiz/views.py
+++ LearningApp/quiz/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from .models import Category, Subject, Question, Answer, Score
 from django.http import Http404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.db import connection  # zeigt queries genauer und wie schnell performed
+from django.db.models import Q  # Keyword argument queries
 
 
 def home(request):
@@ -12,6 +15,7 @@ def about(request):
     return render(request, "quiz/about.html")
 
 
+@login_required
 def categories(request, subject: str):
     if Subject.objects.filter(name__iexact=subject).exists():
         context = {
@@ -27,6 +31,7 @@ def categories(request, subject: str):
         raise Http404("Ooops! Diese Seite existiert leider nicht.")
     
 
+@login_required
 def quiz(request, c_id):
     # if question.is_answered:
     # question = Question.objects.filter(category__id=c_id).except(is_answered=True).first()
@@ -50,6 +55,7 @@ def quiz(request, c_id):
     )
 
 
+@login_required
 def check_answer(request, a_id):
     answer = Answer.objects.get(id=a_id)
     question = answer.question
@@ -80,3 +86,40 @@ def check_answer(request, a_id):
         score.save()
 
     return quiz(request, category.id)
+
+# ORM Tutorial
+
+# OR QUERIES
+##########################################
+#def show_questions_(request):
+    #questions = Question.objects.all()
+
+    #print(questions)
+    #print(questions.query)
+    #print(connection.queries)
+
+    #return render(request, "quiz/about.html", {'questions': questions})
+
+# OR Query
+#def show_questions_(request):
+        #questions = Question.objects.filter(marks=1) | Question.objects.filter(question__startswith=48)
+
+        #print(questions)
+        #print(connection.queries)
+
+        #return render(request, "quiz/about.html", {"questions": questions})
+
+# Q objects: 
+def show_questions(request):
+    questions = Question.objects.filter(Q(marks=1) 
+                                        | Q(question__startswith=48) 
+                                        | ~Q(marks=2))  # not mark
+
+    print(questions)
+    print(connection.queries)
+
+    return render(request, "quiz/about.html", {"questions": questions})
+
+    
+# AND QUERIES
+##########################################
