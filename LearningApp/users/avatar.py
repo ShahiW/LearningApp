d5e1900ebaa543805e6130cd0ab0db185e7b8fa0
Shahi Winderling <s.winderling@posteo.de>
2024-05-15 10:16:03 +0200

    install py-avataaars and create first avatar

---
+++ LearningApp/users/avatar.py
@@ -0,0 +1,8 @@
+# importing the require package
+from py_avataaars import PyAvataaar
+
+# assigning various parameters to our avatar
+avatar = PyAvataaar()
+
+# rendering the avatar in png format
+avatar.render_png_file("AVATAR_1.png")
